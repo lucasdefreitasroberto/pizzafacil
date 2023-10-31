87af007887f3b28ddc82bbeaad65734e78504de7
lucasdefreitasroberto <68399974+lucasdefreitasroberto@users.noreply.github.com>
2023-10-31 16:17:56 -0300

    Contruindo Services/User/CreateUser e Controllers/User/CreateUser

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { Router, Response, Request } from "express";
 
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
 const router = Router();
 
+// --- ROTA TESTE ---
 router.get("/test", (req: Request, res: Response) => {
   //throw new Error("Erro ao fazer essa requisição")
-  return res.json({ nome: "Pizza Fácil" });
+  return res.json({ nome: "TESTE - Pizza Fácil - TESTE" });
 });
+
+
+// --- ROTAS USER ---
+router.get("/users", new CreateUserController().handle)
 
 
 export { router };
